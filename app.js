3ece9538630b31dc33dd246b6daf754d251df32f
esther-v <esvalas@gmail.com>
2021-10-18 17:31:51 +0200

    connection to db ok

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv/config')
+
 
 //Routes
 app.get('/', (req, res) => {
     res.send('We are on home')
 })
 
+app.get('/posts', (req, res) => {
+    res.send('We are on posts')
+})
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log('connected to db')
+})
 //Start listening to the server
 app.listen(3000)
